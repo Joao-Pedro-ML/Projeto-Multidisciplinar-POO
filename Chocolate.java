1a8f2468c1fab1e994b8a06818acd54ffa9d429d
João Pedro Moreto Lourenção <jlourencao@alunos.utfpr.edu.br>
2021-12-03 10:58:51 -0300

    Create Chocolate.java

---
+++ Chocolate.java
@@ -0,0 +1,28 @@
+package Pacote;
+
+public class Chocolate extends Produto{
+	
+	public Chocolate() {
+		
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getPreco() {
+		return preco;
+	}
+
+	public void setPreco(int preco) {
+		this.preco = preco;
+	}
+	
+	
+	
+}
+
