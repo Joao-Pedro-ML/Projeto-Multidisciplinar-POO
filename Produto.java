8c48c7f9c6101053df9dd034429a478274375967
João Pedro Moreto Lourenção <jlourencao@alunos.utfpr.edu.br>
2021-12-03 10:58:33 -0300

    Create Produto.java

---
+++ Produto.java
@@ -0,0 +1,18 @@
+package Pacote;
+
+public class Produto {
+	protected String nome;
+	protected int preco;
+	
+	public Produto() {
+		// TODO Auto-generated constructor stub
+		
+	}
+	
+	
+	public Função_futura_que_nao_decidimos_o_que_faz(){
+		
+	}
+	
+	
+}
