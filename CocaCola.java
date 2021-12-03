68ec361c619c237e18bc2bd1cb40c331cb3cd3f1
João Pedro Moreto Lourenção <jlourencao@alunos.utfpr.edu.br>
2021-12-03 10:59:23 -0300

    Create CocaCola.java

---
+++ CocaCola.java
@@ -0,0 +1,19 @@
+package Pacote;
+
+public class CocaCola extends Produto{
+	
+	
+	
+	public CocaCola(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public int getPreco() {
+		return preco;
+	}
+}
+
